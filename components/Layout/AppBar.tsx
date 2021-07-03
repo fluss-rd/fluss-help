1d64a06b3519061e1e1a1d0dfa833c03044ec17b
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-07-03 02:05:04 -0400

    All links working

--- components/Layout/AppBar.tsx
+++ components/Layout/AppBar.tsx
@@ -1,5 +1,6 @@
 import {
   AppBar as Navbar,
+  Link,
   Button,
   Toolbar,
   Typography,
@@ -10,9 +11,18 @@ import { useRouter } from "next/router";
 import React, { FC } from "react";
 
 const AppBar: FC = () => {
-  const classes = useStyles();
   const router = useRouter();
+  const currentRoute = router.pathname;
+  const classes = useStyles({ currentRoute });
+  const theme = useTheme();
   const goTo = (path: string) => () => router.push(path);
+
+  const computeBackground = (route: string) => {
+    console.log({ currentRoute, route });
+    if (currentRoute === route) return theme.palette.grey[200];
+
+    return undefined;
+  };
 
   return (
     <Navbar position="fixed" color="inherit">
@@ -40,16 +50,31 @@ const AppBar: FC = () => {
             ayuda
           </Typography>
         </Button>
-        <div className={classes.startButtons} />
+        <div className={classes.startButtons}>
+          <Button
+            color="default"
+            className={classes.reportsButton}
+            style={{ background: computeBackground("/") }}
+            onClick={goTo("/")}
+          >
+            Ayuda
+          </Button>
+        </div>
         <div className={classes.endButtons}>
           <Button
             color="default"
             className={classes.reportsButton}
+            style={{ background: computeBackground("/contact") }}
             onClick={goTo("/contact")}
           >
             Contacto
           </Button>
-          <Button color="default" className={classes.reportsButton}>
+          <Button
+            color="default"
+            className={classes.reportsButton}
+            href="https://fluss-bioteam.vercel.app/"
+            target="_blank"
+          >
             Ir a fluss
           </Button>
         </div>
@@ -58,50 +83,52 @@ const AppBar: FC = () => {
   );
 };
 
-const useStyles = makeStyles((theme: Theme) => ({
-  brand: {
-    display: "flex",
-    alignItems: "center",
-    cursor: "pointer",
-    marginRight: theme.spacing(3),
-    padding: 0,
-  },
-  logo: {
-    display: "flex",
-    alignItems: "center",
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-    display: "none",
-    fontSize: theme.typography.h5.fontSize,
-    cursor: "pointer",
-    textTransform: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-    },
-  },
-  startButtons: {
-    flexGrow: 1,
-    display: "flex",
-    alignItems: "center",
-  },
-  reportsButton: {
-    borderRadius: 10,
-  },
-  endButtons: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    "& > *:not(:last-child)": {
+const useStyles = makeStyles<Theme, { currentRoute: string }>(
+  (theme: Theme) => ({
+    brand: {
+      display: "flex",
+      alignItems: "center",
+      cursor: "pointer",
       marginRight: theme.spacing(3),
+      padding: 0,
     },
-  },
-  navbar: {
-    backgroundColor: `${theme.palette.background.default}CC`,
-    backdropFilter: `blur(4px)`,
-  },
-}));
+    logo: {
+      display: "flex",
+      alignItems: "center",
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      display: "none",
+      fontSize: theme.typography.h5.fontSize,
+      cursor: "pointer",
+      textTransform: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block",
+      },
+    },
+    startButtons: {
+      flexGrow: 1,
+      display: "flex",
+      alignItems: "center",
+    },
+    reportsButton: {
+      borderRadius: 10,
+    },
+    endButtons: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      "& > *:not(:last-child)": {
+        marginRight: theme.spacing(3),
+      },
+    },
+    navbar: {
+      backgroundColor: `${theme.palette.background.default}CC`,
+      backdropFilter: `blur(4px)`,
+    },
+  })
+);
 
 export default AppBar;
 
